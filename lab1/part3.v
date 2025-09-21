c3abcf59b3781b80c4ff075d8b5730d444ef0c8d
Harrison Cazzin <hcazzin@bayviewglen.ca>
2025-09-21 11:38:03 -0400

    Small fix.

--- lab1/part3.v
+++ lab1/part3.v
@@ -11,7 +11,7 @@ module lab1part3(SW, LEDR);
 	assign W = SW[1:0];
 	
 	assign T[0] = (~S0[0] & U[0]) | (~S0[0] & V[0]);
-	assign T[1] = (~S0[0] & U[1]) | (~S0[1] & V[1]);
+	assign T[1] = (~S0[1] & U[1]) | (~S0[1] & V[1]);
 	
 	assign M[0] = (~S1[0] & T[0]) | (S1[0] & W[0]);
 	assign M[1] = (~S1[1] & T[1]) | (S1[1] & W[1]);
