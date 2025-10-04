11dd681bfb5bc25c3100482affb126f1a8ce289d
Harrison Cazzin <hcazzin@bayviewglen.ca>
2025-10-04 01:17:02 -0400

    Lab3 Part1 and 2

---
+++ lab3/part2.v
@@ -0,0 +1,18 @@
+module part2 (SW, LEDR);
+    input [1:0] SW;
+    output [9:0] LEDR;
+
+    D_latch D1(SW[1], SW[0], LEDR[1], LEDR[0]);
+
+endmodule;
+
+module D_latch(Clk, D, Qa, Qb);
+    input Clk, D;
+    output Qa, Qb;
+    wire S_g, R_g;
+
+    assign S_g = ~(D & Clk);
+    assign R_g = ~(~D & Clk);
+    assign Qa = ~(S_g & Qb);
+    assign Qb = ~(R_G & Qa);
+endmodule;
---
+++ lab3/part5.v
@@ -0,0 +1,7 @@
+module part5 (SW, KEY, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, LEDR);
+    input [7:0] SW;
+    input [1:0] KEY;
+    output [6:0] HEX5, HEX4, HEX3, HEX2, HEX1, HEX0;
+    output [9:0] LEDR;
+    
+endmodule;
