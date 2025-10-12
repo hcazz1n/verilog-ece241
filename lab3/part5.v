817283ccfc1b91be00e70329ada322f2f2140591
Harrison Cazzin <hcazzin@bayviewglen.ca>
2025-10-12 02:01:03 -0400

    part5 lab 3 done

--- lab3/part5.v
+++ lab3/part5.v
@@ -2,18 +2,15 @@ module part5 (SW, KEY, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, LEDR);
     input [7:0] SW;
     input [1:0] KEY;
     output [6:0] HEX5, HEX4, HEX3, HEX2, HEX1, HEX0;
-    output [9:0] LEDR; 
+    output [9:0] LEDR;
 
     assign clock = KEY[1];
     assign Resetn = KEY[0];
+    assign LEDR[9] = KEY[1];
+    assign LEDR[8] = KEY[0];
 
     wire [7:0] Q, storedQ;
     async_R8 R(SW[7:0], clock, Resetn, Q, storedQ); //sets Q to D if we are assigning values (clock = 1, Resetn = 1) or Q to 0 if we are resetting values (clock = x, Resetn = 0)
-
-    seg7 H3(SW[7:4], HEX3); //first 4bits A
-    seg7 H2(SW[3:0], HEX2); //second 4bits A
-    seg7 H1(SW[7:4], HEX1); //first 4bits B
-    seg7 H0(SW[3:0], HEX0); //second 4bits B
 
     //8bit ripple-carry adder (add from LSB to MSB)
     wire [7:0] S, cout;
@@ -28,10 +25,14 @@ module part5 (SW, KEY, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, LEDR);
 
     assign LEDR[0] = cout[7];
 
-    seg7 H5();
-    seg7 H4();
+    seg7 H3(storedQ[7:4], HEX3); //first 4bits A
+    seg7 H2(storedQ[3:0], HEX2); //second 4bits A
+    seg7 H1(SW[7:4], HEX1); //first 4bits B
+    seg7 H0(SW[3:0], HEX0); //second 4bits B
 
-endmodule;
+    seg7 H5(S[7:4], HEX5);
+    seg7 H4(S[3:0], HEX4);
+endmodule
 
 module async_R8(D, clock, Resetn, Q, storedQ); //asynchronous 8bit register (8bit flip flop). num holds A or B.
     input [7:0] D;
@@ -43,17 +44,18 @@ module async_R8(D, clock, Resetn, Q, storedQ); //asynchronous 8bit register (8bi
     always @ (negedge Resetn, posedge clock) //if Resetn -> 0 OR clock on a positive edge (clock -> 1). Since Resetn is in sensitivity list, it can directly influence the output, i.e. doesn't have to wait for posedge clock to be true before evaluating the statements in the always block.
     begin
         if(!Resetn)
-            storedQ <= D
+            storedQ <= D;
         else
             Q <= D;
     end
-endmodule;
+endmodule
 
 module seg7(SW, HEX);
     input [3:0] SW;
     output [6:0] HEX;
-
-    assign [3:0] X = SW;
+    
+    wire [3:0] X;
+    assign X = SW;
 
     //made k-map of zeros, so ~ whole term around to be active-low instead
     assign HEX[6] = ~((~X[3] & X[2] & ~X[1]) | (X[3] & ~X[2] & ~X[1]) | (X[3] & X[2] & X[0]) | (~X[2] & X[1] & X[0]) | (X[1] & ~X[0]));
@@ -63,7 +65,7 @@ module seg7(SW, HEX);
     assign HEX[2] = ~((~X[3] & ~X[1] & ~X[0]) | (X[3] & ~X[2]) | (~X[1] & X[0]) | (~X[3] & X[2]) | (~X[3] & X[1] & X[0]));
     assign HEX[1] = ~((~X[3] & ~X[1] & ~X[0]) | (X[3] & ~X[2] & ~X[0]) | (X[3] & ~X[1] & X[0]) | (~X[3] & X[1] & X[0]) | (~X[3] & ~X[2]));
     assign HEX[0] = ~((~X[2] & ~X[1] & ~X[0]) | (X[3] & X[2] & ~X[0]) | (X[3] & ~X[2] & ~X[1] & X[0]) | (X[3] & X[1] & ~X[0]) | (X[3] & X[2] & X[1]) | (~X[3] & X[1]) | (~X[3] & X[2] & X[0]));
-endmodule;
+endmodule
 
 module FA (a, b, Cin, S, Cout);
     input a, b, Cin;
