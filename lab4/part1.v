461de68749167e0bc6cdd2b0cb07369d97698a8a
Harrison Cazzin <hcazzin@bayviewglen.ca>
2025-10-18 12:52:13 -0400

    Lab 4 - Relevant stencils created.

---
+++ lab4/part1.v
@@ -0,0 +1,11 @@
+module part1 (SW, KEY, HEX1, HEX0);
+    input [1:0] SW;
+    input [0:0] KEY;
+    output [6:0] HEX1, HEX0;
+
+
+endmodule
+
+module TFF(T, Clock, Q, Clearn);
+    
+endmodule
---
+++ lab4/part3.v
@@ -0,0 +1,6 @@
+module part3 (KEY, CLOCK_50, HEX0);
+input [0:0] KEY;
+input CLOCK_50;
+output [6:0] HEX0;
+
+endmodule
